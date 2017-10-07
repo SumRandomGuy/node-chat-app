2166473de9935682b43d1bc9810c2fe017b2c8be
Sum Random Guy <sumrandomguy@sumrandomguy.com>
2017-10-07 13:58:45 -0500

    username functionality fixed

--- public/js/chat.js
+++ public/js/chat.js
@@ -27,8 +27,6 @@ socket.on('connect', function () {
     } else {
       console.log('No error');
     }
-
-
   });
 });
 
@@ -73,10 +71,9 @@ socket.on('newLocationMessage', function (message) {
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
-
   var messageTextbox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function() {
     messageTextbox.val('');
